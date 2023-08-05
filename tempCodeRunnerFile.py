02e3b53b1ef556878241739b31b881a7e1cd09ca
Nandini Singh <nishasingh5m@gmail.com>
2023-08-05 20:25:48 +0530

    removing tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
-
-popular_df = pickle.load(open('popular_events.pkl', 'rb'))
-Date=(list(popular_df['Date'].values))
-for i in range(len(Date)):
-    print(Date[i])
